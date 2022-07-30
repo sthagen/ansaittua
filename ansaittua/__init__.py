39ea7b5d6d7d54f13b8fa6be18228dd354d65228
Stefan Hagen <stefan@hagen.link>
2022-07-30 16:18:01 +0200

    Bumped impl version of sub MVP for release

--- ansaittua/__init__.py
+++ ansaittua/__init__.py
@@ -15,8 +15,8 @@ DEFAULT_CONFIG_NAME = '.ansaittua.json'
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
-__version__ = '2022.7.28+parent.9fb117a4'
-# [[[end]]] (checksum: a143e8c8551090fa81324b0683ba1a40)
+__version__ = '2022.7.30+parent.cad5c0e2'
+# [[[end]]] (checksum: bf3e4cb130b92c08b23ac44978b7d275)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
